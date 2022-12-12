b94d31ac02c38e855b034482af3b645860c2c39f
RavenHex <69777457+RavenHex1296@users.noreply.github.com>
2022-12-12 22:13:39 +0000

    fixed pseudoinverse

--- pseudoinverse.py
+++ pseudoinverse.py
@@ -7,7 +7,7 @@ def run(x, y):
     n = np.matrix.getI((xt*np.matrix(x)))
     return n*(xt*np.matrix(y))
 
-x = [[1, 1], [2, 1], [3, 1]]
-y = [[math.log(2)], [math.log(5)], [math.log(10)]]
+x = np.matrix('-2 1; 0 1; 1 1')
+y = np.matrix('0.1; 0.7; 0.9')
 
 print(run(x, y))
